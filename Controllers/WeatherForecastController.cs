7c92afde8819edb01ea0f1ed3116a4e744224a7e
Meeran Mydeen S <meeran@buckman.com>
2024-10-15 13:40:23 +0530

    commented the vulnerability method

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -28,30 +28,30 @@ namespace WebApplicationWebAppCheckMarx.Controllers
             return Ok("Completed");
         }
 
-        [HttpPost("request")]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(typeof(ErrorResponse), 500)]
-        public async Task<IActionResult> CreateNewApprovalRequest([FromBody] NewApprovalRequest approvalRequest)
-        {
-            if (approvalRequest == null || !ModelState.IsValid)
-            {
-                return BadRequest("Invalid request data.");
-            }
+        //[HttpPost("request")]
+        //[ProducesResponseType(204)]
+        //[ProducesResponseType(typeof(ErrorResponse), 500)]
+        //public async Task<IActionResult> CreateNewApprovalRequest([FromBody] NewApprovalRequest approvalRequest)
+        //{
+        //    if (approvalRequest == null || !ModelState.IsValid)
+        //    {
+        //        return BadRequest("Invalid request data.");
+        //    }
 
-            HtmlSanitizer sanitizer = new HtmlSanitizer();
-            approvalRequest.Description = sanitizer.Sanitize(approvalRequest.Description);
-            approvalRequest.PONumber = sanitizer.Sanitize(approvalRequest.PONumber);
+        //    //HtmlSanitizer sanitizer = new HtmlSanitizer();
+        //    //approvalRequest.Description = sanitizer.Sanitize(approvalRequest.Description);
+        //    //approvalRequest.PONumber = sanitizer.Sanitize(approvalRequest.PONumber);
 
-            approvalRequest.Items.ForEach(item =>
-            {
-                item.Comment = sanitizer.Sanitize(item.Comment);
-                item.DeliveryAddress = sanitizer.Sanitize(item.DeliveryAddress);
-                item.Supplier = sanitizer.Sanitize(item.Supplier);
-                item.VendorId = sanitizer.Sanitize(item.VendorId);
-            });
+        //    //approvalRequest.Items.ForEach(item =>
+        //    //{
+        //    //    item.Comment = sanitizer.Sanitize(item.Comment);
+        //    //    item.DeliveryAddress = sanitizer.Sanitize(item.DeliveryAddress);
+        //    //    item.Supplier = sanitizer.Sanitize(item.Supplier);
+        //    //    item.VendorId = sanitizer.Sanitize(item.VendorId);
+        //    //});
 
 
-            return Ok("Completed");
-        }
+        //    return Ok("Completed");
+        //}
     }
 }
